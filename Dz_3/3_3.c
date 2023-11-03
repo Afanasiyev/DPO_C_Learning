c130c44157df91f56e98545ce4c51a4f2d01c672
Afanasiyev <wdecay@gmail.com>
2023-11-03 13:12:20 +0300

    Dz_3 add

---
+++ Dz_3/3_3.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Input number:\n");
+    scanf ("%d%d", &a, &b);
+    c = a - b ;
+    printf("%d\n", c);
+    return 0;
+}
---
+++ Dz_3/3_4.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c ;
+    float d;
+    printf("Input number:\n");
+    scanf ("%d%d%d", &a, &b, &c);
+    d = (float)(a + b + c)/3 ;
+    printf("%.2f\n", d);
+    return 0;
+}
